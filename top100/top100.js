c0aef9b8c0e47e799b298b7ea20d34958b032a68
Edison Arango <edisonarab@gmail.com>
2021-10-04 23:59:00 -0500

    Feature/preloding images

--- top100/top100.js
+++ top100/top100.js
@@ -3,16 +3,27 @@ const COPYRIGHT_URL = BASE_URL + "copyright/";
 const JSON_FEED = BASE_URL + "images/d2d/?order_by=-priority";
 const XML_FEED = BASE_URL + "images/feed/top100/";
 const LOGO_SRC = "img/noirlab-white.svg";
+const STORED_IMAGE_KEY = "stored_image";
 const logoLinkElement = document.querySelector("#logo-link");
 const logoElement = document.querySelector("#logo");
 const titleElement = document.querySelector("#title");
 const creditElement = document.querySelector("#copyright");
 const className = "imageTop100";
+let loadedFromStorage = false;
 
 function initialize() {
   logoLinkElement.href = BASE_URL;
   logoElement.src = LOGO_SRC;
   creditElement.href = COPYRIGHT_URL;
+  if (localStorage.getItem(STORED_IMAGE_KEY)) {
+    try {
+      const preloadedImage = JSON.parse(localStorage.getItem(STORED_IMAGE_KEY));
+      displayImage(preloadedImage);
+      loadedFromStorage = true;
+    } catch {
+      // The normal process will load an image anyway
+    }
+  }
   getImagesFromJSON();
 }
 
@@ -33,14 +44,27 @@ function createImageFromJSON(image) {
   return img;
 }
 
+function displayImage(image) {
+  document.body.appendChild(createImageFromJSON(image));
+}
+
+function onLoadedJSON(images) {
+  let randomInt = getRandomInt(0, 99);
+  if (!loadedFromStorage) {
+    displayImage(images[randomInt]);
+  }
+  // Preload next image
+  randomInt = getRandomInt(0, 99);
+  const nextImg = images[randomInt];
+  localStorage.setItem(STORED_IMAGE_KEY, JSON.stringify(nextImg));
+  createImageFromJSON(nextImg);
+}
+
 function getImagesFromJSON() {
-  const randomInt = getRandomInt(0, 99);
   fetch(JSON_FEED)
     .then((res) => res.json())
     .then((out) => {
-      document.body.appendChild(
-        createImageFromJSON(out.Collections[randomInt])
-      );
+      onLoadedJSON(out.Collections);
     })
     .catch((err) => console.error(err));
 }

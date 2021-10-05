926a7ff1565a7bda75eeca00e3ae77b853f24af0
Edison Arango <edisonarab@gmail.com>
2021-10-04 21:36:22 -0500

    Fix/XML no longer required

--- top100/top100.js
+++ top100/top100.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://noirlab.edu/public/";
 const COPYRIGHT_URL = BASE_URL + "copyright/";
-const JSON_FEED = BASE_URL + "images/archive/top100/json/";
+const JSON_FEED = BASE_URL + "images/d2d/?order_by=-priority";
 const XML_FEED = BASE_URL + "images/feed/top100/";
 const LOGO_SRC = "img/noirlab-white.svg";
 const logoLinkElement = document.querySelector("#logo-link");
@@ -24,7 +24,7 @@ function getRandomInt(min, max) {
 
 function createImageFromJSON(image) {
   const img = document.createElement("img");
-  img.src = image.Resources[2].URL;
+  img.src = image.Assets[0].Resources[2].URL;
   img.alt = image.Title;
   img.className = className;
   titleElement.textContent = image.Title;
@@ -34,38 +34,15 @@ function createImageFromJSON(image) {
 }
 
 function getImagesFromJSON() {
+  const randomInt = getRandomInt(0, 99);
   fetch(JSON_FEED)
     .then((res) => res.json())
     .then((out) => {
-      console.log(out[getRandomInt(0, 99)]);
-      document.body.appendChild(createImageFromJSON(out[getRandomInt(0, 99)]));
+      document.body.appendChild(
+        createImageFromJSON(out.Collections[randomInt])
+      );
     })
     .catch((err) => console.error(err));
-}
-
-function createImageTagFromXML(image) {
-  const img = document.createElement("img");
-  img.src = image.enclosure[0]._attr.url._value;
-  img.alt = image.title[0]._text;
-  img.className = className;
-  titleElement.textContent = image.title[0]._text;
-  titleElement.href = image.guid[0]._text;
-  return img;
-}
-
-function getImagesFromXMLFile() {
-  let request = (XMLFile = new XMLHttpRequest());
-  request.open("GET", XML_FEED);
-  request.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      let rawdata = xmlToJSON.parseString(this.response);
-      let images = rawdata.rss[0].channel[0].item;
-      document.body.appendChild(
-        createImageTagFromXML(images[getRandomInt(1, 99)])
-      );
-    }
-  };
-  request.send();
 }
 
 initialize();

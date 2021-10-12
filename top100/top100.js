fac7614e90761dba9354232eb841e2ffe2790a79
Edison Arango <edisonarab@gmail.com>
2021-10-11 21:20:45 -0500

    Feature/rewritten entirely using Vue3 render function due to Chrome CSP and using chrome.storage

--- top100/top100.js
+++ top100/top100.js
@@ -17,59 +17,139 @@ function URLfromImage(image) {
   return image.Assets[0].Resources[2].URL;
 }
 
-PetiteVue.createApp({
-  loading: false,
-  loadedFromStorage: false,
-  config: CONFIG,
-  image: null,
-  get imageURL() {
-    if (this.image) {
+// Abstraction of storage to use chrome storage when installed as extension
+// or local storage when deployed as a normal webpage
+const storage = {
+  getItem(key) {
+    return new Promise((resolve, reject) => {
       try {
-        return URLfromImage(this.image);
+        // See: https://developer.chrome.com/docs/extensions/reference/storage/
+        chrome.storage.local.get([key], function (result) {
+          resolve(result[key]);
+        });
       } catch {
-        console.error("Failed to get image URL");
+        resolve(JSON.parse(localStorage.getItem(key)));
       }
-    }
-    return null;
+    });
   },
-  initialize() {
-    if (localStorage.getItem(CONFIG.STORED_IMAGE_KEY)) {
-      try {
-        const preloadedImage = JSON.parse(
-          localStorage.getItem(CONFIG.STORED_IMAGE_KEY)
-        );
-        this.image = preloadedImage;
-        this.loadedFromStorage = true;
-      } catch {
-        // The normal process will load an image anyway
-        console.error("Failed to load stored image");
+  setItem(key, value) {
+    try {
+      const obj = {};
+      obj[key] = value;
+      chrome.storage.local.set(obj);
+    } catch {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  },
+};
+
+// It's not possible to use the HTML templates because it would require the Vue version with the compiler
+// The compiler version uses "eval" which is not allowed by the CSP of Chrome (See: https://developer.chrome.com/docs/extensions/mv3/sandboxingEval/)
+// No even the chrome sandbox works becuase it means we won't be able to use chrome.storage
+// That's the reason we manually wrote Vue's render function
+Vue.createApp({
+  render() {
+    const h = Vue.h;
+    return [
+      this.loading &&
+        h("div", { class: "loader-wrapper" }, h("div", { class: "loader" })),
+      h("div", { class: "metadata-wrapper" }, [
+        h(
+          "div",
+          { class: "logo-wrapper" },
+          h(
+            "a",
+            { href: this.config.BASE_URL, target: "_blank" },
+            h("img", { src: this.config.LOGO_SRC, class: "logo" })
+          )
+        ),
+        this.image &&
+          h("div", {}, [
+            h(
+              "a",
+              {
+                href: this.image.ReferenceURL,
+                alt: this.image.Title,
+                class: "title",
+                target: "_blank",
+              },
+              this.image.Title
+            ),
+            h(
+              "a",
+              {
+                href: this.config.COPYRIGHT_URL,
+                target: "_blank",
+              },
+              this.image.Credit
+            ),
+          ]),
+      ]),
+      this.imageURL && h("img", { src: this.imageURL, class: "the-image" }),
+    ];
+  },
+  data() {
+    return {
+      loading: false,
+      loadedFromStorage: false,
+      config: CONFIG,
+      image: null,
+    };
+  },
+  computed: {
+    imageURL() {
+      if (this.image) {
+        try {
+          return URLfromImage(this.image);
+        } catch {
+          console.error("Failed to get image URL");
+        }
       }
-    } else {
+      return null;
+    },
+  },
+  // Async can be used since Chrome v55. See: https://caniuse.com/async-functions
+  async mounted() {
+    try {
+      const storedImage = await storage.getItem(CONFIG.STORED_IMAGE_KEY);
+      if (storedImage && storedImage.Title) {
+        // Evaluate if at least it has the title to avoid empty objects
+        this.image = storedImage;
+        this.loadedFromStorage = true;
+      } else {
+        this.loading = true;
+      }
+    } catch (e) {
+      console.log(e);
+      // The normal process will load an image anyway
+      console.error("Failed to load stored image");
       this.loading = true;
     }
     this.getImagesFromJSON();
   },
-  getImagesFromJSON() {
-    fetch(CONFIG.JSON_FEED)
-      .then((res) => res.json())
-      .then((out) => {
-        this.onJSONloaded(out.Collections);
-      })
-      .catch((err) => console.error(err))
-      .finally(() => {
-        this.loading = false;
-      });
+  methods: {
+    getImagesFromJSON() {
+      fetch(CONFIG.JSON_FEED)
+        .then((res) => res.json())
+        .then((out) => {
+          this.onJSONloaded(out.Collections);
+        })
+        .catch((err) => console.error(err))
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    onJSONloaded(images) {
+      if (!this.loadedFromStorage) {
+        this.image = images[getRandomInt(0, images.length)];
+      }
+      this.preloadNextImage(images);
+    },
+    preloadNextImage(images) {
+      const nextImg = images[getRandomInt(0, images.length)];
+      storage.setItem(CONFIG.STORED_IMAGE_KEY, nextImg);
+      const img = document.createElement("img");
+      img.src = URLfromImage(nextImg);
+    },
   },
-  onJSONloaded(images) {
-    if (!this.loadedFromStorage) {
-      this.image = images[getRandomInt(0, images.length)];
-    }
-    this.preloadNextImage(images);
-  },
-  preloadNextImage(images) {
-    const nextImg = images[getRandomInt(0, images.length)];
-    localStorage.setItem(CONFIG.STORED_IMAGE_KEY, JSON.stringify(nextImg));
-    const img = document.createElement("img");
-    img.src = URLfromImage(nextImg);
-  },
-}).mount();
+}).mount("#app");
